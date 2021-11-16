fe7268bf171c106f20b54b2dd42f35247d5f479b
mzalot <69329263+mzalot@users.noreply.github.com>
2021-11-16 14:19:06 -0700

    added a header

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,2 +1,10 @@
+/**
+ * PrefixCalculator
+ * Makes a prefix calculator
+ * Author: August Penny and Mareks Zeile
+ * Collaborator(s):
+ * Collaboration:
+ * Date: 11/16/21
+ **/
 public class PostfixCalculator {
 }
