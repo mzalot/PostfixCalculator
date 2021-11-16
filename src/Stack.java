9b28d685ce0347224bb83f355ab150e454fac0ad
August Penny <apenny23@apenny23.kentdenver.org>
2021-11-16 14:15:43 -0700

    Added the stack and the node class

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,186 @@
-public class Stack {
+/**
+ * Stack.java
+ * Makes a stack using the Node clas
+ * Author: August Penn
+ * Collaborator(s): The names of anyone you collaborated with here
+ * Collaboration: Describe the collaboration that took place
+ * Date: Date the program is handed in 11/11/21
+ **/
+
+public class Stack<E, T> {
+
+    private int length;
+
+    private node root;
+
+    public Stack(){
+        root=new node();
+        length=0;
+    }
+
+
+    //private methods to make the public ones work.
+
+    private node topNode(node n2){//recursive method to return the current top of the stack
+        if(n2.getPrev()==null){
+            return n2;
+        }
+
+        return topNode(n2.getPrev());
+    }
+
+
+    public void push(E element){//method to make a node with inputted value and add it on to the top of the chain
+        node n2=new node(element,null,null);
+
+        if(length==0){//if the first value, sets the value of the root node to the input value
+
+            root.setDataType(element);
+
+        }
+
+
+        if(length==1){//if second value to be added, directly interacts with the root.
+
+
+            n2.setNext(root);
+
+
+            root.setPrev(n2);
+
+
+        }
+
+
+        if(length>1){//if more than second value to be added, uses recursive method.
+
+
+            n2.setNext(topNode(root));
+
+
+            topNode(root).setPrev(n2);
+
+
+
+
+
+
+
+        }
+
+
+        length++;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public T peek(){
+
+
+        return (T) topNode(root).getValue();//return the value of the top node in the stack casted to a T type
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int size(){//returns the length counter that is in the class instance data.
+
+
+        return length;
+
+
+    }
+
+
+
+
+
+
+
+    public boolean isEmpty(){
+
+
+        if(length==0&&root.getValue()==null){//checks if it only has the root node and if the root node is null or if it has a value.
+
+
+            return true;
+
+
+        }
+
+
+        return false;
+
+
+    }
+
+
+    public T pop(){
+
+
+        if (length==0){
+
+
+            return null;
+
+
+        }
+
+
+        node temp = topNode(root);
+
+
+        node temp2 = topNode(root).getNext();//gets a temp node at the second to top position (will be top when the method finishes)
+
+
+        temp2.setPrev(null);
+
+
+        length--;
+
+
+        return (T) temp.getValue();//return the value of the top node in the stack casted to a T type
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
+
+
--- src/node.java
+++ src/node.java
@@ -1,2 +1,57 @@
-public class node {
+
+/**
+ * myNode
+ * A class to make nodes that connect to one another and store data
+ * Author: August Penny
+ * Collaborator(s): The names of anyone you collaborated with here
+ * Collaboration: Describe the collaboration that took place
+ * Date: Date the program is handed in: 9/29/21
+ **/
+
+public class node<T> {
+
+    private T val; //data stored
+    private node Next; //child node
+    private node Prev; //parent node
+
+
+    public node(){//default constructor
+        val=null;
+        Next = null;
+    }
+    public node(T t, node n, node p){//constructor with refrence node and value
+        this.val=t;
+        this.Next=n;
+        this.Prev=p;
+    }
+    public T getValue(){//retuens value
+        return val;
+    }
+    public node getNext(){//returns child node
+        return Next;
+    }
+    public node getPrev(){//returns previous node
+        return Prev;
+    }
+
+    public void setNext(node n) {//sets connected node
+        Next = n;
+    }
+    public void setPrev(node n){
+        Prev=n;
+    }
+    public void setDataType(T t){//sets value
+        this.val=t;
+    }
+    public String toString(){ //returns the nodes data in the tostring with prefix "Node :"
+        return "Node: " + val.toString();
+    }
+    public boolean equals(node n){
+        if (n.getValue().equals(this.getValue())){
+            return true;
+        }
+        return false;
+    }
+
+
 }
