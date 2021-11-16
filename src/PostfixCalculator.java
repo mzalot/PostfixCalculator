d9b20295d5b24a8e70565c87a246a95c0ced2d98
mzalot <69329263+mzalot@users.noreply.github.com>
2021-11-16 14:40:23 -0700

    fix

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -7,7 +7,7 @@
  * Date: 11/16/21
  **/
 public class PostfixCalculator {
-<<<<<<< HEAD
+
     //instance data
     Stack s;
     String input;
@@ -17,8 +17,5 @@ public class PostfixCalculator {
         s = new Stack();
         this.input = input;
     }
-=======
 
-
->>>>>>> 9c1c092f18329b8d70121effab6bc3f34ba7c069
 }
