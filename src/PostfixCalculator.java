403f290aa6ed27aac2454b9d679b13a2d791a011
mzalot <69329263+mzalot@users.noreply.github.com>
2021-11-24 13:18:39 -0600

    added to toStack method

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -30,6 +30,7 @@ public class PostfixCalculator {
         for (int i=str.length()-1;i>=0;i=i-2){
             System.out.print(str.charAt(i));
             ret.push(str.charAt(i));
+            counter++;
         }
         System.out.println();
         s=ret;
